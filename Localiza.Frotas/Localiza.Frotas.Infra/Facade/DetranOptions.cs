63826c18cea032b349f8e50f0bce93fd65d939f6
prote home <https://github.com/Josewesley2020/CatalogoDeJogos.git>
2021-12-17 11:02:26 -0300

    Primeira Versao

---
+++ Localiza.Frotas/Localiza.Frotas.Infra/Facade/DetranOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Localiza.Frotas.Infra.Facade
+{
+    public class DetranOptions
+    {
+        public Guid Id { get;} = Guid.NewGuid();
+
+        public string BaseUrl { get; set; }
+        public string VistoriaUrl { get; set; }
+        public int QuantidadeDiasParaAgendamento { get; set; }
+    }
+}
